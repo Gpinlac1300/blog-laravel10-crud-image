f3f7b589e9689bdcf15341a2dd2ed514d9f7ffc5
arlou777 <104336082+arlou777@users.noreply.github.com>
2024-05-13 23:06:11 +0800

    first commit

---
+++ resources/views/components/textarea-input.blade.php
@@ -0,0 +1,5 @@
+<x-textarea-input disabled>
+    @props(['disabled' => false])
+    <textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm'])
+    !!}>{{$slot}}</textarea>
+</x-textarea-input>
